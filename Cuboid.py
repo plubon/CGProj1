258798e06a7f548b0dcf5a17e242e6f72e028ab3
Piotr Luboń <plubon1337@gmail.com>
2016-04-05 01:23:46 +0200

    fix kolorow w median cutcie

--- Cuboid.py
+++ Cuboid.py
@@ -4,7 +4,7 @@ class Cuboid:
         self.r = r
         self.g = g
         self.b = b
-        self.color = (self.r[1]-self.r[0], self.g[1]-self.g[0], self.b[1]-self.b[0])
+        self.color = ((self.r[1]+self.r[0])/2, (self.g[1]+self.g[0])/2, (self.b[1]+self.b[0])/2)
 
     def containspixel(self, pix):
         return pix[0] >= self.r[0] and pix[0] <= self.r[1] and pix[1] >= self.g[0] and pix[1] <= self.g[1] and pix[2] >= self.b[0] and pix[2] <= self.b[1]
